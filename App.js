7a57be8a4ff2ab4ed7d1a5681728d98c2a8b4208
Matheus Paiva <m.bacakhau@gmail.com>
2022-12-04 11:44:17 -0300

    feat: api

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import api from './src/service/api';
+import Filmes from './src/screens/index';
+
+class App extends Component{
+  
+  constructor(props){
+    super(props);
+
+    this.state ={
+      filmes: []
+    }
+  }
+
+  async componentDidMount(){
+    const response = await api.get('r-api/?api=filmes');
+    this.setState({
+      filmes: response.data
+    })
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <FlatList
+        data={this.state.filmes}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item}) => <Filmes data={item}/>}
+        />
+      </View>
+    )
+  }
+
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 100,
   },
 });
